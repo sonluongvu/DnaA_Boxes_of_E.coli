78437c28dd0fc648d41b1a9d03438f92f4cd1eda
sonluongvu <sonluong999@gmail.com>
2019-05-14 17:20:16 +0700

    Creating #G-#C versus k data points

--- Finding_DnaA_boxes.py
+++ Finding_DnaA_boxes.py
@@ -1,2 +1,21 @@
-from Bio import SeqIO
-record = SeqIO.read("GCF_000005845.2_ASM584v2_genomic.fna","fasta")
+
+Ecoli_genome_file = open('DnaA_Boxes_of_E.coli/E.coli_genome.txt')
+Ecoli_genome = list(Ecoli_genome_file.read())
+
+
+
+GC_graph = []
+k = 0
+GC_count = 0;
+
+for nucleotide in Ecoli_genome:
+    k+=1
+    if nucleotide == 'G':
+        GC_count += 1
+        GC_graph.append([k,GC_count])
+    elif nucleotide == 'C':
+        GC_count -= 1
+        GC_graph.append([k,GC_count])
+    else:
+         GC_graph.append([k,GC_count])
+
