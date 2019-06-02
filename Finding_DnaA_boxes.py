209df5e117f3c86089088fe559759367d0f0a642
sonluongvu <sonluong999@gmail.com>
2019-06-02 13:53:00 +0700

    Update Finding_DnaA_boxes.py

--- Finding_DnaA_boxes.py
+++ Finding_DnaA_boxes.py
@@ -1,14 +1,12 @@
 
-Ecoli_genome_file = open('DnaA_Boxes_of_E.coli/E.coli_genome.txt')
-Ecoli_genome = list(Ecoli_genome_file.read())
-
-
+Ecoli_genome_file = open('DnaA_Boxes_of_E.coli/E.coli_genome.txt') # Open genome sequence
+Ecoli_genome = list(Ecoli_genome_file.read()) # Create a genome sequence list
 
 GC_graph = []
 k = 0
-GC_count = 0;
+GC_count = 0
 
-for nucleotide in Ecoli_genome:
+for nucleotide in Ecoli_genome: # Create a list containing k and #G-#C data points
     k+=1
     if nucleotide == 'G':
         GC_count += 1
